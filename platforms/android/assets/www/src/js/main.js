f390f327d9e156ff1580ae1a678b0f104cb8e6ad
Max <maksymkharuk@iesebre.com>
2016-04-24 20:17:43 +0200

    Added mobile controls

---
+++ platforms/android/assets/www/src/js/main.js
@@ -0,0 +1,17 @@
+
+var game;
+
+
+//game = new Phaser.Game(5000, 1500, Phaser.AUTO, 'Game');
+game = new Phaser.Game(900, 500, Phaser.AUTO, 'Game');
+
+// First parameter is how our state will be called.
+// Second parameter is an object containing the needed methods for state functionality
+
+game.state.add('boot', boot);
+game.state.add('menu', menu);
+game.state.add('levelPassed', levelPassed);
+game.state.add('gameWin', gameWin);
+game.state.add('playStateLevel1', playStateLevel1);
+game.state.add('playStateLevel2', playStateLevel2);
+game.state.start('boot');
